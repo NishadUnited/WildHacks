1f8157149428afa0fe21305a7e0764bd6185859e
Pranay Prakash <pranay.gp@gmail.com>
2015-11-22 01:21:15 -0600

    Find by x vlue instead of id

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -72,7 +72,7 @@ class FlagsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_flag
-      @flag = Flag.find(params[:id])
+      @flag = Flag.find_by :x params[:id]
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
